4e55ed7926eef62897825761646a27f997d18e60
Popkid-254 <popkidtechs@gmail.com>
2025-04-23 08:43:49 +0300

    Update repo.js

--- plugins/repo.js
+++ plugins/repo.js
@@ -6,7 +6,7 @@ const repo = async (m, gss) => {
   const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(" ")[0].toLowerCase() : "";
 
   if (["repo", "sc", "script", "info"].includes(cmd)) {
-    const githubRepoURL = "https://github.com/devpopkid/POPKID-BMX";
+    const githubRepoURL = "https://github.com/Popkiddevs/POPKID-XTECH";
 
     try {
       // Extract username and repo name from the URL
